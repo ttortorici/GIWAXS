730053445486000a2fa04b8b6465669786d5a279
Teddy Tort <etc.tortorici@gmail.com>
2024-02-28 15:29:15 -0700

    a

--- stitch.py
+++ stitch.py
@@ -1,4 +1,8 @@
 from pyFAI.detectors import Eiger1M
+from pathlib import Path
+import numpy as np
+import fabio
+import matplotlib.pylab as plt
 
 
 class Detector(Eiger1M):
@@ -39,29 +43,31 @@ class Stitcher:
     def __init__(self, stitch_num, exposure_time):
         self.stitch_num = stitch_num
         self.exposure_time = exposure_time
-        self.detector = Eiger1M()
+        self.detector = Detector()
         self.size = self._determine_size()
 
-    def _determine_size(self, size) -> tuple:
+    def _determine_size(self) -> tuple:
         rows = self.detector.get_rows() + Stitcher.DEAD_BAND_PIXELS
         columns = self.stitch_num * (self.detector.get_columns() - Stitcher.COLUMN_OVERLAP) + Stitcher.COLUMN_OVERLAP
         return (rows, columns)
     
-    def load_data(self):
-        
+    def load_data(self, directory: Path):
+        data = np.empty(self.size)
+        mask = np.empty(self.size)
+        for scol in range(1, self.stitch_num + 1):
+            for srol in range(1, 3):
+                file = directory / f"1_{scol}_{srol}"
+                file_data = fabio.open(file).data
+                file_mask = self.detector.calc_mask()
+                file_data *= np.logical_not(file_mask)
+                # file_mask[np.where(file_data) < 0] = 1
+
+
+
+                
 
 
 
 if __name__ == "__main__":
-    import matplotlib.pylab as plt
-    import numpy as np
-
-    detector = Detector()
-    print(detector.get_size())
-    # print(detector)
-    # print(detector.mask)
-    # print(np.logical_not(detector.mask))
-    # print(np.sum(detector.mask))
-    # print(37 * 1030)
-    # plt.imshow(detector.calc_mask())
-    # plt.show()
+    stitcher = Stitcher(3, 10)
+    stitcher.load_data(Path("D:\\OneDrive - UCB-O365\\Rogerslab3\\Teddy\\TPP Films\\BTB-TPP\\2024 Film Growth\\Film 1\\GIWAXS TT5-06"))
